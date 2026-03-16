f4fe1467c011c3a44bafb9785c0414255ec558c4
Rush Garris <rushtong06@gmail.com>
2026-03-16 11:41:30 -0400

    finsihed mage progression

--- lib/data/gear_data_mage.dart
+++ lib/data/gear_data_mage.dart
@@ -1,3 +1,220 @@
+import '../models/gear_item.dart';
 import '../models/gear_loadout.dart';
 
-const Map<String, GearLoadout> mageLoadouts = {};
+const Map<String, GearLoadout> mageLoadouts = {
+  'pre_moon_lord': GearLoadout(
+    weapons: [
+      GearItem(name: 'Razorpine / Razorblade Typhoon'),
+      GearItem(name: 'Nebula Blaze'),
+      GearItem(name: 'Nebula Arcanum'),
+      GearItem(name: "Betsy's Wrath"),
+    ],
+    armor: [
+      GearItem(name: 'Spectre Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Terraspark Boots'),
+      GearItem(name: 'T6 Wings (Check Notes)'),
+      GearItem(name: 'Master Ninja Gear'),
+      GearItem(name: 'Celestial Emblem'),
+      GearItem(name: 'Mana Cloak'),
+      GearItem(name: 'Sorcerer Emblem'),
+      GearItem(name: 'Soaring Insignia'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'n/a'),
+    ],
+  ),
+
+  'pre_lunar_events': GearLoadout(
+    weapons: [
+      GearItem(name: 'Razorpine / Razorblade Typhoon'),
+      GearItem(name: 'Nightglow / Stellar Tune'),
+      GearItem(name: 'Laser Machinegun / Charged Blaster Cannon'),
+      GearItem(name: "Betsy's Wrath"),
+    ],
+    armor: [
+      GearItem(name: 'Spectre Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Terraspark Boots'),
+      GearItem(name: 'T6 Wings (Check Notes)'),
+      GearItem(name: 'Master Ninja Gear'),
+      GearItem(name: 'Celestial Emblem'),
+      GearItem(name: 'Mana Cloak'),
+      GearItem(name: 'Sorcerer Emblem'),
+      GearItem(name: 'Soaring Insignia'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'n/a'),
+    ],
+  ),
+
+  'pre_golem': GearLoadout(
+    weapons: [
+      GearItem(name: 'Razorpine'),
+      GearItem(name: 'Bat Scepter / Spectre Staff'),
+      GearItem(name: 'Shadowbeam Staff / Inferno Fork'),
+      GearItem(name: 'Magnet Sphere / Vampire Knives'),
+    ],
+    armor: [
+      GearItem(name: 'Spectre Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Terraspark Boots'),
+      GearItem(name: 'T4/T5 Wings (Check Notes)'),
+      GearItem(name: 'Master Ninja Gear'),
+      GearItem(name: 'Celestial Emblem'),
+      GearItem(name: 'Mana Cloak'),
+      GearItem(name: 'Sorcerer Emblem'),
+      GearItem(name: 'Ankh Shield / Charm of Myths'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'n/a'),
+    ],
+  ),
+
+  'pre_plantera': GearLoadout(
+    weapons: [
+      GearItem(name: 'Magical Harp'),
+      GearItem(name: 'Venom Staff / Tome of Infinite Wisdom'),
+      GearItem(name: 'Rainbow Rod'),
+      GearItem(name: 'Golden Shower'),
+    ],
+    armor: [
+      GearItem(name: 'Hallowed / Chlorophyte Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Terraspark Boots'),
+      GearItem(name: 'T2/T3 Wings (Check Notes)'),
+      GearItem(name: 'Shield of Cthulhu'),
+      GearItem(name: 'Celestial Emblem'),
+      GearItem(name: 'Mana Cloak'),
+      GearItem(name: 'Sorcerer Emblem'),
+      GearItem(name: 'Ankh Shield / Charm of Myths'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'Life Fruit (20)'),
+      GearItem(name: 'Aegis Fruit'),
+    ],
+  ),
+
+  'pre_mech_bosses': GearLoadout(
+    weapons: [
+      GearItem(name: 'Sky Fracture / Meteor Staff'),
+      GearItem(name: 'Killing Deck / Crystal Serpent'),
+      GearItem(name: 'Nimbus Rod / Clinger Staff'),
+      GearItem(name: 'Golden Shower / Life Drain'),
+    ],
+    armor: [
+      GearItem(name: 'Titanium / Adamantite Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Terraspark Boots'),
+      GearItem(name: 'T1 Wings (Check Notes)'),
+      GearItem(name: 'Shield of Cthulhu'),
+      GearItem(name: 'Celestial Cuffs'),
+      GearItem(name: 'Mana Cloak'),
+      GearItem(name: 'Sorcerer Emblem'),
+      GearItem(name: 'Ankh Shield / Charm of Myths'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'Demon Heart'),
+    ],
+  ),
+
+  'pre_evil_boss': GearLoadout(
+    weapons: [
+      GearItem(name: 'Demon Scythe'),
+      GearItem(name: 'Thunder Zapper'),
+      GearItem(name: 'Gray Zapinator'),
+      GearItem(name: 'Crimson Rod / Vilethorn'),
+    ],
+    armor: [
+      GearItem(name: 'Jungle Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Terraspark Boots'),
+      GearItem(name: 'Bundle of Horseshoe Balloons'),
+      GearItem(name: 'Shield of Cthulhu'),
+      GearItem(name: 'Celestial Cuffs'),
+      GearItem(name: 'Magnet Flower'),
+      GearItem(name: 'Frog Gear / Magiluminescence'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'n/a'),
+    ],
+  ),
+
+  'pre_skeletron': GearLoadout(
+    weapons: [
+      GearItem(name: 'Bee Gun'),
+      GearItem(name: 'Demon Scythe / Thunder Zapper'),
+      GearItem(name: 'Gray Zapinator / Space Gun'),
+      GearItem(name: 'Crimson Rod'),
+    ],
+    armor: [
+      GearItem(name: 'Jungle / Meteor Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Terraspark Boots'),
+      GearItem(name: 'Bundle of Horseshoe Balloons'),
+      GearItem(name: 'Shield of Cthulhu'),
+      GearItem(name: 'Celestial Cuffs'),
+      GearItem(name: 'Magnet Flower'),
+      GearItem(name: 'Worm Scarf / Brain of Confusion'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'n/a'),
+    ],
+  ),
+
+  'pre_wall_of_flesh': GearLoadout(
+    weapons: [
+      GearItem(name: 'Aqua Scepter'),
+      GearItem(name: 'Book of Skulls / Water Bolt'),
+      GearItem(name: 'Flower of Fire'),
+      GearItem(name: 'Magic Missile / Flamelash'),
+    ],
+    armor: [
+      GearItem(name: 'Jungle Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Terraspark Boots'),
+      GearItem(name: 'Bundle of Horseshoe Balloons'),
+      GearItem(name: 'Shield of Cthulhu'),
+      GearItem(name: 'Celestial Cuffs'),
+      GearItem(name: 'Magnet Flower'),
+      GearItem(name: 'Obsidian Shield'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'n/a'),
+    ],
+  ),
+
+  'pre_eye': GearLoadout(
+    weapons: [
+      GearItem(name: 'Demon Scythe'),
+      GearItem(name: 'Thunder Zapper'),
+      GearItem(name: 'Diamond Staff / Ruby Staff'),
+      GearItem(name: 'Wand of Frosting'),
+    ],
+    armor: [
+      GearItem(name: 'Jungle Armor'),
+    ],
+    accessories: [
+      GearItem(name: 'Dunerider Boots'),
+      GearItem(name: 'Sandstorm in a Bottle / Fledgling Wings'),
+      GearItem(name: 'Shark Tooth Necklace'),
+      GearItem(name: "Nature's Gift / Celestial Magnet"),
+      GearItem(name: 'Frog Leg / Shiny Red Balloon'),
+      GearItem(name: 'Anklet of the Wind / Band of Regeneration'),
+    ],
+    permUpgrades: [
+      GearItem(name: 'Life Crystal (15)'),
+      GearItem(name: 'Vital Crystal'),
+      GearItem(name: 'Mana Crystal (9)'),
+      GearItem(name: 'Arcane Crystal'),
+    ],
+  ),
+};
